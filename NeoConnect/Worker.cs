761da7689e37fe77fef98838acf5dd7c864bdc1c
Matt King <Matthew.King@thomsonreuters.com>
2025-07-29 15:20:28 +0100

    Made Schedule optional and updated readme

--- NeoConnect/Worker.cs
+++ NeoConnect/Worker.cs
@@ -4,18 +4,13 @@ namespace NeoConnect
 {
     public class Worker : BackgroundService
     {
-        private const string schedule = "0 2 * * *"; // every day at 2am
+        private readonly ActionsService _actionsService;
+        private readonly IConfiguration _config;
 
-        private readonly ILogger<Worker> _logger;     
-        private readonly ActionsService _actionsService;              
-        private readonly CronExpression _cron;
-
-        public Worker(ILogger<Worker> logger, IConfiguration config, ActionsService actionsService)
+        public Worker(IConfiguration config, ActionsService actionsService)
         {
-            _logger = logger;
+            _config = config;
             _actionsService = actionsService;
-
-            _cron = CronExpression.Parse(config["ExecutionSchedule"] ?? "0 2 * * *"); // default schedule: every day at 2am
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,21 +19,27 @@ namespace NeoConnect
             Console.WriteLine(@"| \ | | ___  ___  / ___|___  _ __  _ __   ___  ___| |_ ");
             Console.WriteLine(@"|  \| |/ _ \/ _ \| |   / _ \| '_ \| '_ \ / _ \/ __| __|");
             Console.WriteLine(@"| |\  |  __/ (_) | |__| (_) | | | | | | |  __/ (__| |_ ");
-            Console.WriteLine(@"|_| \_|\___|\___/ \____\___/|_| |_|_| |_|\___|\___|\__|");            
+            Console.WriteLine(@"|_| \_|\___|\___/ \____\___/|_| |_|_| |_|\___|\___|\__|");
             Console.WriteLine("");
-            Console.WriteLine("Waiting for next scheduled run. Press any key to run immediately.");
-            Console.WriteLine("");
+
+            var schedule = _config["Schedule"];
+            CronExpression? _cron = null;                        
+            if (schedule == null || !CronExpression.TryParse(schedule, CronFormat.Standard, out _cron))
+            {
+                // No schedule is configured, run immediately.
+                await _actionsService.PerformActions(stoppingToken);
+                return;
+            }
 
             while (!stoppingToken.IsCancellationRequested)
             {
                 var utcNow = DateTime.UtcNow;
-                var nextUtc = _cron.GetNextOccurrence(utcNow) ?? utcNow;
-                                
-                //await Task.Delay(nextUtc - utcNow, stoppingToken);                
-                await Task.WhenAny(Task.Delay(nextUtc - utcNow, stoppingToken), Task.Run(Console.ReadKey, stoppingToken));
+                var nextRunUtc = _cron.GetNextOccurrence(utcNow) ?? utcNow;
+                
+                Console.WriteLine("Next run scheduled for " + nextRunUtc.ToString("G"));
 
-                await _actionsService.PerformActions(stoppingToken);
-
+                await Task.Delay(nextRunUtc - utcNow, stoppingToken);                
+                
                 Console.WriteLine("");
             }
         }
